4f2fa2faa0a1aaae4fe6546085ccc8aa606b53cc
KsZero111 <khanhbui.dubai@gmail.com>
2022-10-19 08:03:38 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 const data = require('./database/connect')
 
 //get user
-// data.query('delete from category returning *')
-//     .then( result => console.log(result.rows) )
-
-
-data.query(`insert into users(username, password) values ('demo', '123123') returning *`)
+data.query('delete from supplier returning *')
     .then( result => console.log(result.rows) )
+
+
+// data.query(`insert into users(username, password) values ('demo', '123123') returning *`)
+//     .then( result => console.log(result.rows) )
 // data.query(`insert into users(username, password) values ('canthoshop', '123123') returning *`)
 //     .then( result => console.log(result.rows) )
 
